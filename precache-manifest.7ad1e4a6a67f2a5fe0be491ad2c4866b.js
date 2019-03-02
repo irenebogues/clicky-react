8d4436965bb9eaceb2bfbe306fec873797807cde
irenebogues <irenebogues@gmail.com>
2019-03-02 18:23:05 -0500

    Updates

---
+++ precache-manifest.7ad1e4a6a67f2a5fe0be491ad2c4866b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "33f4baa6113db21cf888",
+    "url": "/clicky-react/static/css/main.07f3e5d8.chunk.css"
+  },
+  {
+    "revision": "33f4baa6113db21cf888",
+    "url": "/clicky-react/static/js/main.33f4baa6.chunk.js"
+  },
+  {
+    "revision": "8287c1bf6dc503db5c9f",
+    "url": "/clicky-react/static/js/runtime~main.8287c1bf.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/clicky-react/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "d1a40fa46839c513688137c9c0cb50b1",
+    "url": "/clicky-react/index.html"
+  }
+];
