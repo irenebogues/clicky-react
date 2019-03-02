dd5d08fb0f15c5c2993f02969ee92bb5ca66a63a
irenebogues <irenebogues@gmail.com>
2019-03-02 17:49:52 -0500

    using friend card exercise for boiler plate

--- src/App.js
+++ src/App.js
@@ -1,66 +1,37 @@
-// import React, { Component } from 'react';
-// import logo from './logo.svg';
-// import './App.css';
-
-// class App extends Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <header className="App-header">
-//           <img src={logo} className="App-logo" alt="logo" />
-//           <p>
-//             Edit <code>src/App.js</code> and save to reload.
-//           </p>
-//           <a
-//             className="App-link"
-//             href="https://reactjs.org"
-//             target="_blank"
-//             rel="noopener noreferrer"
-//           >
-//             Learn React
-//           </a>
-//         </header>
-//       </div>
-//     );
-//   }
-// }
-
-// export default App;
-
 import React, { Component } from "react";
-import avatarcard from "./components/avatarcard";
+import FriendCard from "./components/FriendCard";
 import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
-import avatars from "./avatar.json";
+import friends from "./friends.json";
 import "./App.css";
 
 class App extends Component {
-  // Setting this.state.avatars to the avatars json array
+  // Setting this.state.friends to the friends json array
   state = {
-    avatars
+    friends
   };
 
-  removeAvatar = id => {
-    // Filter this.state.avatars for avatars with an id not equal to the id being removed
-    const avatars = this.state.avatars.filter(avatar => avatar.id !== id);
-    // Set this.state.avatars equal to the new avatars array
-    this.setState({ avatars });
+  removeFriend = id => {
+    // Filter this.state.friends for friends with an id not equal to the id being removed
+    const friends = this.state.friends.filter(friend => friend.id !== id);
+    // Set this.state.friends equal to the new friends array
+    this.setState({ friends });
   };
 
-  // Map over this.state.avatars and render a avatarCard component for each avatar object
+  // Map over this.state.friends and render a FriendCard component for each friend object
   render() {
     return (
       <Wrapper>
-        <Title>Avatar List</Title>
-        {this.state.avatars.map(avatar => (
-          <avatarCard
-            removeAvatar={this.removeAvatar}
-            id={avatar.id}
-            key={avatar.id}
-            name={avatar.name}
-            image={avatar.image}
-            professions={avatar.occupation}
-            location={avatar.location}
+        <Title>Friends List</Title>
+        {this.state.friends.map(friend => (
+          <FriendCard
+            removeFriend={this.removeFriend}
+            id={friend.id}
+            key={friend.id}
+            name={friend.name}
+            image={friend.image}
+            profession={friend.occupation}
+            location={friend.location}
           />
         ))}
       </Wrapper>
@@ -69,4 +40,3 @@ class App extends Component {
 }
 
 export default App;
-
